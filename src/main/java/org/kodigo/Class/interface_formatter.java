dc616dd93e0b636e6cf85c98563fb6861f3dbd23
Roberto1313 <67878026+Roberto1313@users.noreply.github.com>
2022-11-22 10:58:13 -0600

    Update interface_formatter.java
    Añadí la nueva función para formato de fechas para las facturas

--- src/main/java/org/kodigo/Class/interface_formatter.java
+++ src/main/java/org/kodigo/Class/interface_formatter.java
@@ -2,6 +2,7 @@ package org.kodigo.Class;
 
 public interface interface_formatter {
     public String formatDate();
+    public Date formatDateToFactures(String format);
     public String Hour();
     public boolean mail( String formatMail);
 }
