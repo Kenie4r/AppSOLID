ca4bf7fee433030a939ff0b06b5515272152e223
Keneth Nolasco <54483710+Kenie4r@users.noreply.github.com>
2022-11-23 22:02:55 -0600

    Creación de cuerpo de EMAIL HTML

---
+++ src/main/java/org/kodigo/Utils/MailCreator.java
@@ -0,0 +1,36 @@
+package org.kodigo.Utils;
+
+import org.kodigo.Class.Invoice;
+
+
+
+
+public class MailCreator implements MailCreatorInterface{
+
+
+//    A JAVA class that have the job to make a HTML body in order to have the classes more comfortable to edit in a future
+
+
+    @Override
+    public boolean makeBodyForMail(Invoice invoice) {
+
+
+//    Just create the String data to the body
+        String htmlBODY = "<html>" +
+                "<body>" +
+                "<h2>FACTURA DE SERVICIO</h2>" +
+            "<p>Hola cliente, por este medio te compartimos las facturas de tu hogar," +
+            " recuerda que puedes pagar estos servicios en las ventanillas del banco, etc. </p>" +
+            invoice.toString()+
+            "</body>" +
+            "</html>";
+//      Call the mail request function in order to send the email
+        return makeRequestForMail(htmlBODY);
+}
+
+    @Override
+    public boolean makeRequestForMail(String body) {
+        //Llamar el objeto de mail para poder enviar el dato
+        return false;
+    }
+}
