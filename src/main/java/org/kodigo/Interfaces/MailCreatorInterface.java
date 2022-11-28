b2c8c6b87b824fdfdafba6dd9641d935b09c1e2a
Keneth Nolasco <54483710+Kenie4r@users.noreply.github.com>
2022-11-28 10:11:15 -0600

    Actualización de email

--- src/main/java/org/kodigo/Interfaces/MailCreatorInterface.java
+++ src/main/java/org/kodigo/Interfaces/MailCreatorInterface.java
@@ -1,9 +1,11 @@
 package org.kodigo.Interfaces;
 
+import org.kodigo.Class.Casa;
 import org.kodigo.Class.Invoice;
+import org.kodigo.Class.Person;
 
 public interface MailCreatorInterface {
-    public boolean makeBodyForMail(Invoice invoice);
-    public boolean makeRequestForMail(String body);
+    public boolean makeBodyForMail(Invoice invoice, Casa casa);
+    public boolean makeRequestForMail(String body, Person person);
 
 }
--- src/main/java/org/kodigo/Utils/MailCreator.java
+++ src/main/java/org/kodigo/Utils/MailCreator.java
@@ -1,6 +1,9 @@
 package org.kodigo.Utils;
 
+import org.kodigo.Class.Casa;
+import org.kodigo.Class.Charge;
 import org.kodigo.Class.Invoice;
+import org.kodigo.Class.Person;
 import org.kodigo.Interfaces.MailCreatorInterface;
 
 
@@ -11,25 +14,31 @@ public class MailCreator implements MailCreatorInterface {
 
 
     @Override
-    public boolean makeBodyForMail(Invoice invoice) {
-
+    public boolean makeBodyForMail(Invoice invoice,  Casa casa) {
 
 //    Just create the String data to the body
         String htmlBODY = "<html>" +
                 "<body>" +
                 "<h2>FACTURA DE SERVICIO</h2>" +
-            "<p>Hola cliente, por este medio te compartimos las facturas de tu hogar," +
-            " recuerda que puedes pagar estos servicios en las ventanillas del banco, etc. </p>" +
-            invoice.toString()+
-            "</body>" +
-            "</html>";
+                "<p>Hola cliente, por este medio te compartimos las facturas de tu hogar," +
+                " recuerda que puedes pagar estos servicios en las ventanillas del banco, etc. </p>" +
+                "<table> <caption>Factura de "+invoice.getService().getProveedor()+"</caption><thead><tr><th>Nombre de cargo </th> <th>Coste </th></tr></thead>";
+        for (Charge cargo :
+                invoice.getCharges()) {
+            htmlBODY+="<tr><td>"+cargo.getName()+"</td><td>"+cargo.getPrice()+"</td></tr>";
+        }
+        htmlBODY+="<tr><td>Total: </td><td>"+invoice.getTotal()+"</td></tr>";
+
+        htmlBODY+="</table>     </body>" +
+                "</html>";
 //      Call the mail request function in order to send the email
-        return makeRequestForMail(htmlBODY);
+        return makeRequestForMail(htmlBODY, casa.getPropietario());
 }
 
     @Override
-    public boolean makeRequestForMail(String body) {
+    public boolean makeRequestForMail(String body, Person person) {
         //Llamar el objeto de mail para poder enviar el dato
+        Mail
         return false;
     }
 }
