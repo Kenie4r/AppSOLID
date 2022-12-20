4fd7b1309476a0bc6bf549ea3ae850b7b7849095
DiegoMolina95 <72415724+DiegoMolina95@users.noreply.github.com>
2022-12-19 22:20:51 -0600

    Update Logger.java
    MInor change

--- src/main/java/org/kodigo/Utils/Logger.java
+++ src/main/java/org/kodigo/Utils/Logger.java
@@ -7,8 +7,8 @@ public class Logger {
     private static final org.apache.logging.log4j.Logger LOGGER = LogManager.getLogger(Logger.class);
 
     public static void main(String[] args) {
-        LOGGER.info("This is a New INFO level on The main Project!");
-        LOGGER.error("This is an ERROR level on the main Project");
+        LOGGER.info("This is a New INFO  on The main Project!");
+        LOGGER.error("This is an ERROR  on the main Project");
     }
 
     public static void logInfo(String s){
