f89e67570e2c46138d512f6347c1b4c475466c27
Roberto1313 <67878026+Roberto1313@users.noreply.github.com>
2022-12-21 17:57:24 -0600

    Create FormatterTest.java

---
+++ src/test/java/FormatterTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import org.kodigo.Utils.Formatter;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class FormatterTest {
+
+    @Test
+    public  void testMail(){
+        boolean result = Formatter.mail("roberto@gmail.com");
+        assertTrue(result);
+    }
+}
