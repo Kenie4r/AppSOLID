d7a04d4cd1658236b061f26e85bb3bbd009d4a3d
Roberto1313 <67878026+Roberto1313@users.noreply.github.com>
2022-12-21 17:54:33 -0600

    Update interface_formatter.java

--- src/main/java/org/kodigo/Interfaces/interface_formatter.java
+++ src/main/java/org/kodigo/Interfaces/interface_formatter.java
@@ -6,5 +6,4 @@ public interface interface_formatter {
     public String formatDate();
     public Date formatDateToFactures(String format);
     public String Hour();
-    public boolean mail( String formatMail);
 }
