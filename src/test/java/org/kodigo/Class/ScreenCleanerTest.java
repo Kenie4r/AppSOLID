ec47b5dfac951953578646dbfb6bc9d0a7dc34ff
DiegoMolina95 <72415724+DiegoMolina95@users.noreply.github.com>
2022-12-22 03:41:55 -0600

    Updated Dat of Log and Made ScreenCleaner Test

---
+++ src/test/java/org/kodigo/Class/ScreenCleanerTest.java
@@ -0,0 +1,15 @@
+package org.kodigo.Class;
+
+import org.junit.jupiter.api.Test;
+import org.kodigo.Utils.ScreenCleaner;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class ScreenCleanerTest {
+
+    @Test
+    void testCleanScreen(){
+        assertAll(ScreenCleaner::cleanScreen);
+    }
+
+}
